fb81be9efce7510f18fe93a98ca64eb91b4efcab
Kiran Machineni <mkrn@live.in>
2023-02-04 00:24:39 -0500

    add documentation to api

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const cache = apicache.middleware;
 
 
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes")
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/routes/swagger");
 
 const PORT = process.env.PORT || 3000
 
@@ -14,4 +15,5 @@ app.use("/api/v1/workouts", v1WorkoutRouter)
 
 app.listen(PORT, () => {
 	console.log(`API is listening on port ${PORT}`)
+	V1SwaggerDocs(app, PORT);
 })
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -10,6 +10,29 @@ const router = express.Router()
 // cacheing single request
 // router.get("/", cache("2 minutes"), workoutController.getAllWorkouts);
 
+
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: "#/components/schemas/Workout"
+ */
 router.get("/", workoutController.getAllWorkouts)
 
 router.get("/:workoutId", workoutController.getOneWorkout)
